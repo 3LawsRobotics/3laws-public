8921c7aee408f4d4fdb9e1698a203d3976a3d38d
Thomas Gurriet <tgurriet@3laws.io>
2023-05-05 18:53:52 +0200

    Make sure docker mounts the right volumes

--- rdm/install.sh
+++ rdm/install.sh
@@ -395,12 +395,14 @@ fi
       echo -e "  $SUDO systemctl start ${SRVNAME}"
       cout "To deactivate the daemon on boot, run:"
       echo -e "  $SUDO systemctl disable ${SRVNAME}"
+      cout "To activate the daemon on boot, run:"
+      echo -e "  $SUDO systemctl enable ${SRVNAME}"
 
     else
       cout "In that case, run the following commands to start the docker container:"
-      echo -e "docker run -it --rm --name -v /etc/machine-id:/3laws_robotics/machine-id 3laws_rdm $DOCKER_IMAGE_LINK"
+      echo -e "docker run -it --rm --name 3laws_rdm -v /etc/machine-id:/3laws_robotics/machine-id -v /proc/stat:/3laws_robotics/proc/stat $DOCKER_IMAGE_LINK"
       cout "If you want to run the container in detached mode:"
-      echo -e "docker run -d --rm --name -v /etc/machine-id:/3laws_robotics/machine-id 3laws_rdm $DOCKER_IMAGE_LINK"
+      echo -e "docker run -d --rm --name 3laws_rdm -v /etc/machine-id:/3laws_robotics/machine-id -v /proc/stat:/3laws_robotics/proc/stat $DOCKER_IMAGE_LINK"
     fi
 
   fi

3524d113d0242d10ce50f99ec99703f07644f1e8
Thomas Gurriet <tgurriet@3laws.io>
2023-05-05 17:34:07 +0200

    Updated docker command

--- rdm/install.sh
+++ rdm/install.sh
@@ -398,9 +398,9 @@ fi
 
     else
       cout "In that case, run the following commands to start the docker container:"
-      echo -e "docker run -it --rm --name 3laws_rdm $DOCKER_IMAGE_LINK"
+      echo -e "docker run -it --rm --name -v /etc/machine-id:/3laws_robotics/machine-id 3laws_rdm $DOCKER_IMAGE_LINK"
       cout "If you want to run the container in detached mode:"
-      echo -e "docker run -d --rm --name 3laws_rdm $DOCKER_IMAGE_LINK"
+      echo -e "docker run -d --rm --name -v /etc/machine-id:/3laws_robotics/machine-id 3laws_rdm $DOCKER_IMAGE_LINK"
     fi
 
   fi

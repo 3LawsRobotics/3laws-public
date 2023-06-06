99b06393f770b8575cc685151e4651ee38631da3
Pierre Quebriac <pquebriac@3lawsrobotics.com>
2023-06-06 16:34:25 -0700

    update scan cript

--- rdm/ros_graph_discovery.sh
+++ rdm/ros_graph_discovery.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+
 echo "======= TOPICS ========" > ros_network_info.txt
 echo >> ros_network_info.txt
+echo "starting scan"
 
 # Get the list of topics
 topics=$(ros2 topic list)
@@ -9,6 +11,7 @@ topics=$(ros2 topic list)
 # Iterate through each topic
 for topic in $topics
 do
+  echo "exploring topic: $topic"
   echo "Topic: $topic" >> ros_network_info.txt
   echo "-----------------" >> ros_network_info.txt
 
@@ -28,6 +31,7 @@ nodes=$(ros2 node list)
 # Iterate through each topic
 for node in $nodes
 do
+  echo "exploring node: $node"
   echo "Node: $node" >> ros_network_info.txt
   echo "-----------------" >> ros_network_info.txt
 
@@ -36,3 +40,5 @@ do
 
   echo "=================" >> ros_network_info.txt
 done
+
+echo "End of scan, results saved in ros_network_info.txt"
